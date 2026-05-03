33ca19ee985a0e158a4c80afd846455fbd1d4274
Azizbek Abdullayev <azizbekabdullayev3400@gmail.com>
2026-05-03 10:48:19 +0500

    Remove unnecessary GUI and audio libraries for deployment

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:azizbek2012@localhost:5432/educode_db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:azizbek2012@localhost:5432/educode_db")
+
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
