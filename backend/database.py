0da668e8d329ad2eed70d1ee0d4d757449841ff0
Azizbek Abdullayev <azizbekabdullayev3400@gmail.com>
2026-05-03 11:48:58 +0500

    Fixed...

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:azizbek2012@localhost:5432/educode_db")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:azizbek2012@localhost:5432/educode_hub")
 
 if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
